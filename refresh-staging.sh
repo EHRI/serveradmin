cc2c12b3ae81d8e6e3dab863e123254e18790649
Mike Bryant <mikesname@gmail.com>
2021-01-07 12:10:05 +0000

    Make some improvements to the sync script

--- refresh-staging.sh
+++ refresh-staging.sh
@@ -2,41 +2,54 @@
 
 set -e
 
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
+if [[ $EUID != 0 ]]; then
+    echo "Please run using sudo"
     exit 1
 fi
 
+if [ -z "$PGHOST" ]; then
+  echo "Error: PGHOST not set in environment"
+  exit 1
+fi
 
 NEO4J_BACKUP=$1
-
-if [ ! -f $NEO4J_BACKUP ]; then
+if [ ! -f "$NEO4J_BACKUP" ]; then
     echo "Arg should be a backup file!"
     exit 1
 fi
 
-
-NOW=`date +%Y%m%d%H%M%S`
+NOW=$(date +%Y%m%d%H%M%S)
 DB_DIR=/opt/webapps/data/neo4j/databases/graph.db
-DUMP=$HOME/stage-backup-$NOW.pg_dump
-export PGHOST=ehri-web-01
+BACKUP=$HOME/stage-backup-$NOW
 
-# backup PostgresQL:
+# backup PostgresQL as a restore-friendly dump and also as INSERT statements...
 echo Backing up existing staging PostgreSQL database...
-pg_dump --format=c --create --user=docview_stage --host=$DB_HOST docview_stage > $DUMP
+pg_dump --create --user=docview_stage docview_stage --file "$BACKUP.pg_dump" --format=custom
+pg_dump --create --user=docview_stage docview_stage --file "$BACKUP.sql.gz" --format=plain --inserts --compress=9
 
 # load from prod
 echo Loading PostgreSQL from prod...
-# NB: This only works if the table schema has been 
+# Drop the table schema first...
 psql -e docview_stage -U docview_stage -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 pg_dump --no-owner --no-acl --user=docview docview | psql --user=docview_stage docview_stage
 
-echo Updating Neo4j...
-tar zcf $HOME/stage-backup-$NOW-graph.db.tar.gz -C $DB_DIR $DB_DIR && \
-    sudo rm -rf $DB_DIR/* && \
-    sudo service neo4j-service stop && \
-    tar zxf $NEO4J_BACKUP -C $DB_DIR && \
-    sudo chown neo4j.webadm -R $DB_DIR && \
-    sudo service neo4j-service start
+echo "Updating Neo4j..."
+tar zcf "$HOME/stage-backup-$NOW-graph.db.tar.gz" -C $DB_DIR $DB_DIR
 
-echo Done
+echo "Deleting existing DB $DB_DIR..."
+rm -rf ${DB_DIR:?}/*
+
+echo "Stopping service..."
+service neo4j-service stop
+
+echo "Extracting database $NEO4J_BACKUP to $DB_DIR..."
+tar zxf "$NEO4J_BACKUP" -C $DB_DIR
+
+echo "Setting permissions..."
+chown neo4j.webadm -R $DB_DIR
+
+echo "Restarting Neo4j..."
+service neo4j-service start
+
+echo "Done"
+exit
